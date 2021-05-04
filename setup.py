94e5ba015fd84df9d96453a8768e95420ea33f2a
Marc Arbesman <marbesman@gmail.com>
2021-05-04 08:19:20 -0400

    updates requirements and readme

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 install_requires = [
     'requests>=2.13.0',
 ]
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='zebulon',
-    version='0.0.1',
+    version='1.0.0',
     author='Marc Arbesman',
     author_email='marbesman@gmail.com',
     url='https://github.com/willcode4food/zebulon',
@@ -21,8 +21,8 @@ setup(
     #     'test': tests_require,
     # },
     description='A theory on crypto trading',
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     # download_url='https://github.com/danpaquin/coinbasepro-python/archive/master.zip',
 
 
