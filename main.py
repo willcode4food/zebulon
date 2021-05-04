74bb897e63c470f89ef6b91a1412cb56a6641fc1
Marc Arbesman <marbesman@gmail.com>
2021-05-04 07:17:49 -0400

    Listen timer socket (#3)
    * adds event handler to cbp socket client
    
    * moves subscribers to higher level class
    
    * re-arranges file organization
    
    * adds timer to main
    message emits an index
    
    * cleanup
    adds readme
    
    * ignore output files

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import os
+import csv
+
+from datetime import datetime
+from threading import Timer
+
+import time
+
 
 from CoinbaseProClient import CBPRestClient
 from CoinbaseProClient import CBPSocketClient
@@ -9,6 +16,8 @@ api_secret = os.environ.get('COINBASE_SECRET')
 api_passphrase = os.environ.get('COINBASE_PASSPHRASE')
 
 
+# Uncomment for REST Client
+
 # fetch = CBPRestClient(api_key, api_secret, api_passphrase)
 
 # response = fetch('GET', 'product_trades_btc', '').json()
@@ -18,8 +27,27 @@ socket = CBPSocketClient(api_key, api_secret, api_passphrase)
 
 
 @socket.subscribe
-def on_message(message):
-    print(message)
+def on_message(message, message_index):
+
+    if message_index == 2:
+        header = message.keys()
+
+        csv_writer.writerow(header)
+    values = message.values()
+    csv_writer.writerow(message.values())
+    print(values)
 
 
+def exitfunc():
+    csv_writer.writerow("Exit Time" + str(datetime.now()))
+    trades.close()
+    os._exit(0)
+
+
+# run for 900 seconds (15 minutes)
+Timer(900, exitfunc).start()
+
+trades = open('trades.csv', 'w')
+csv_writer = csv.writer(trades)
+print("Start Time", datetime.now())
 socket(["BTC-USD"])
